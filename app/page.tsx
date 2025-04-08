78b4b48cb0a01b46689aae6c6390071b602e2d4f
loreslei <133262137+loreslei@users.noreply.github.com>
2025-04-08 19:25:49 -0300

    solved small bug and removed vercel logo

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,6 @@ export default function Page() {
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
-        <div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black" />
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
             <Image
             src="/hero-desktop.png"
